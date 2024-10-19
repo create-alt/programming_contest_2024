56d033d786ab679c75ae032a956d21d7be8056a8
create-alt <134464021+create-alt@users.noreply.github.com>
2024-10-19 14:54:27 +0900

    Update main_algo.cpp

--- New_algo/main_algo.cpp
+++ New_algo/main_algo.cpp
@@ -4,11 +4,38 @@ using namespace std;
 
 
 vector<vector<int>> board, goal;
+vector<bool> num_apper = [false, false, false, false]
 
 void main_algo() {
+	bool chain_eq = true;
 	for (int y = 0; y < board[0].size(); y++) {
 		for (int x = 0; x < board.size(); x++) {
 
+			if (board[y][x] == goal[y][x] and chain_eq){
+				//左上から確認していき、連続している間はpass
+				break;
+			}
+
+			//違う箇所が現れたらそれ以降のpieceから同じ値を探索
+			chain_eq = false;
+			num_apper[board[y][x]] = true;
+
+			goal_piece = goal[y][x];
+			int column_num = 0;
+			for(int y_sel = y + 1; y_sel<board[0].size(); y_sel++){
+				column_num++;
+				
+				int raw_num = 0;
+				for (int x_sel = x + 1; x_sel < board.size(); x_sel++) {
+					raw_num++;
+					if goal_piece == board[y_sel][x_sel]{
+						int column_cut_size = 0;
+						
+					    //
+					}
+
+				}
+			}
 		}
 	}
 }
