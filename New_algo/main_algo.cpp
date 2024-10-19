f8a892c072524824b1a14bf9c84f72ad5630afae
create-alt <134464021+create-alt@users.noreply.github.com>
2024-10-19 14:31:12 +0900

    Create main_algo.cpp

---
+++ New_algo/main_algo.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<vector<int>> board, goal;
+
+void main_algo() {
+	for (int y = 0; y < board[0].size(); y++) {
+		for (int x = 0; x < board.size(); x++) {
+
+		}
+	}
+}
+
+int main() {
+	board, goal = //通信およびboard取得用の外部クラスを作成
+
+	
+}
