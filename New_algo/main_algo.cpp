68cd4117802915d0a020d0d147ae7d53ab99a35f
create-alt <134464021+create-alt@users.noreply.github.com>
2024-10-19 15:47:32 +0900

    Update main_algo.cpp

--- New_algo/main_algo.cpp
+++ New_algo/main_algo.cpp
@@ -2,46 +2,90 @@
 #include <vector>
 using namespace std;
 
+// json用ライブラリの読み込みを行い、下記を使用可能に
+// 使用できるようになるまではvectorで代替
+//  json send_json = {{"n",0},
+//                    {"ops", {}}};
+
+int n = 0; // 手数
 
 vector<vector<int>> board, goal;
-vector<bool> num_apper = [false, false, false, false]
+vector<vector<int>> ops;
 
-void main_algo() {
-	bool chain_eq = true;
-	for (int y = 0; y < board[0].size(); y++) {
-		for (int x = 0; x < board.size(); x++) {
+vector<int> main_algo()
+{
+    vector<int> act(4);
 
-			if (board[y][x] == goal[y][x] and chain_eq){
-				//左上から確認していき、連続している間はpass
-				break;
-			}
+    bool chain_eq = true;
+    int pos_x, pos_y, cutter, direct;
+    for (int y = 0; y < board[0].size(); y++)
+    {
+        // board全体に対してサーチ
+        for (int x = 0; x < board.size(); x++)
+        {
 
-			//違う箇所が現れたらそれ以降のpieceから同じ値を探索
-			chain_eq = false;
-			num_apper[board[y][x]] = true;
+            if (board[y][x] == goal[y][x] and chain_eq)
+            {
+                // 左上から確認していき、連続している間はpass
+                break;
+            }
 
-			goal_piece = goal[y][x];
-			int column_num = 0;
-			for(int y_sel = y + 1; y_sel<board[0].size(); y_sel++){
-				column_num++;
-				
-				int raw_num = 0;
-				for (int x_sel = x + 1; x_sel < board.size(); x_sel++) {
-					raw_num++;
-					if goal_piece == board[y_sel][x_sel]{
-						int column_cut_size = 0;
-						
-					    //
-					}
+            // 違う箇所が現れたらそれ以降のpieceから同じ値を探索
+            chain_eq = false;
 
-				}
-			}
-		}
-	}
+            int goal_piece = goal[y][x];
+            int column_num = 0; // 列方向の移動数
+
+            //(x,y)に寄せるピースを探索
+            for (int y_sel = y; y_sel < board[0].size(); y_sel++)
+            {
+
+                int raw_num = 0; // 行方向の移動数
+                for (int x_sel = x + 1; x_sel < board.size(); x_sel++)
+                {
+                    raw_num++;
+
+                    // 寄せるピースが見つかったらそのピースを正しく移動させるための行動を選択
+                    if (goal_piece == board[y_sel][x_sel])
+                    {
+                        // int column_cut_size = 0;
+
+                        act[0] = 0;            // cutterの種類（ひとまず簡単のため[[1]]で考える）
+                        act[1], act[2] = x, y; // 抜き型の起点となる座標（できれば負の数から適応も考える）
+
+                        for (int i = 0; i < column_num; i++)
+                        {
+                            act[3] = 0; // 移動方向：上
+                            n++;        // 手数を更新
+
+                            ops.push_back(act); // vector: opsに行動を記録
+                            action(board, act); // 選択した行動を行いboardを更新（boardは参照渡し）
+                        }
+
+                        for (int i = 0; i < raw_num; i++)
+                        {
+                            act[3] = 2; // 移動方向：左
+                            n++;        // 手数を更新
+
+                            ops.push_back(act); // vector: opsに行動を記録
+                            action(board, act); // 選択した行動を行いboardを更新（boardは参照渡し）
+                        }
+                    }
+                }
+                column_num++;
+            }
+        }
+    }
 }
 
-int main() {
-	board, goal = //通信およびboard取得用の外部クラスを作成
+int main()
+{
+    board, goal = // 通信およびboard取得用の外部クラスを作成
 
-	
+    main_algo() // アルゴリズムを実行して行動を取得
+
+    /*
+    以下で、jsonファイルの作成と提出を行う。
+    nとopsはグローバル変数
+    */
 }
